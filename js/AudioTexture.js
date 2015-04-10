bdfe8fd0e96c511db7f0e26f817d78cef3ffb07a
Andrea Hawksley <hawksley@gmail.com>
2015-04-10 14:28:06 -0700

    some code cleanup

--- js/AudioTexture.js
+++ js/AudioTexture.js
@@ -1,12 +1,11 @@
-function AudioTexture( audio ){
-
+function AudioTexture( audio ) {
     var analyser = audio.analyser;
 
     var fbc = analyser.frequencyBinCount;
 
     // TODO: why 2 * 4 instead of 4 ?
     // fudge factor is to make sure texture reachs from 0 -> 1 in vUv coords
-    var pixels = fbc / 8.2; 
+    var pixels = fbc / 8.2;
 
     //creates a canvas element
     var canvas              = document.createElement('canvas');
@@ -16,10 +15,10 @@ function AudioTexture( audio ){
 
     // uncomment to see texture in upper left corner
     //document.body.appendChild( this.canvas );
-    
+
     canvas.width = pixels;
     canvas.height = 1;
-    
+
     var c = canvas.getContext('2d');
 
     var width = canvas.width;
@@ -37,29 +36,26 @@ function AudioTexture( audio ){
     texture.width = width;
     texture.height = height;
 
-   // console.log( audio );
-
     return texture ;
 
   }
 
-  textureUpdate = function(){
-
-    if( this.analyser){
+  textureUpdate = function() {
+    if (this.analyser) {
 
       var imageData = this.c.createImageData( this.width , this.height );
 
       //transfers audio data to rgb values
       for (var i = 0; i < this.pixels ; i++) {
-        
+
         var x = i;
         var y = 0;
         var r = this.analyser.array[i]   | 0;
-        var g = this.analyser.array[i+1] | 0;       
+        var g = this.analyser.array[i+1] | 0;
         var b = this.analyser.array[i+2] | 0;
         var a = this.analyser.array[i+3] | 0;
-        setPixelData( imageData , x , y , r , g , b , a ); 
-      
+        setPixelData( imageData , x , y , r , g , b , a );
+
       }
 
 
@@ -69,15 +65,11 @@ function AudioTexture( audio ){
       this._needsUpdate =  true;
 
     }
-
-    //console.log('ss');
-
   }
 
   //TODO: Move this to canvasFunctions
 
   setPixelData = function ( imageData, x, y, r, g, b, a) {
-
       index = ( x + y * imageData.width ) * 4;
       imageData.data[index+0] = r;
       imageData.data[index+1] = g;
